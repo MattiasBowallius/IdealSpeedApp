2c3d9e8866a4c4a8b3fd2a05ff339901205757cc
Mattias Bowallius <mattias.bowallius@hiq.se>
2015-12-04 14:20:35 +0100

    Added watch

--- Watch Extension/GlanceController.swift
+++ Watch Extension/GlanceController.swift
@@ -7,31 +7,45 @@
 //
 
 import WatchKit
+import WatchConnectivity
 import Foundation
 
 
-class GlanceController: WKInterfaceController {
-
-    @IBOutlet var distanceLable: WKInterfaceLabel!
+class GlanceController: WKInterfaceController, WCSessionDelegate{
+    
+    @IBOutlet var distanceLabel: WKInterfaceLabel!
     @IBOutlet var speedUpImage: WKInterfaceImage!
     @IBOutlet var slowDownImage: WKInterfaceImage!
     @IBOutlet var speedLabel: WKInterfaceLabel!
     
-    override func awakeWithContext(context: AnyObject?) {
-        super.awakeWithContext(context)
-        
-        // Configure interface objects here.
-        slowDownImage.description = CGAffineTransformMakeRotation(CGFloat(M_PI))
+    var session: WCSession!
+    
+    override func didAppear() {
+        if (WCSession.isSupported()) {
+            session = WCSession.defaultSession()
+            session.delegate = self
+            session.activateSession()
+            if(session.reachable){
+                session.sendMessage([:], replyHandler: {(response) -> Void in
+                    print("success")
+                    if let speed = response["speed"] as? String{
+                        self.speedLabel.setText(speed)
+                    }
+                    
+                    }, errorHandler: {(error) -> Void in
+                        print("\(error)")
+                })
+            }else{
+                print("Phone not reachable")
+            }
+        }
     }
-
-    override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
-        super.willActivate()
-    }
-
+    
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
-
+    func sessionReachabilityDidChange(session: WCSession) {
+        print("Reachability is: \(session.reachable)")
+    }
 }
